c9f0da3f426e2a011c3d6b2ba05cc3cecc5a940e
Padraic O'Neill <pconeill@gmail.com [core]>
2018-03-10 19:37:24 -0700

    update seed file creation of semesters, courses, and semester_courses. add current:bool to semester

--- db/migrate/20180311021813_create_semesters.rb
+++ db/migrate/20180311021813_create_semesters.rb
@@ -2,7 +2,7 @@ class CreateSemesters < ActiveRecord::Migration[5.1]
   def change
     create_table :semesters do |t|
       t.string :term
-
+      t.boolean :current
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180311021913) do
 
   create_table "semesters", force: :cascade do |t|
     t.string "term"
+    t.boolean "current"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
